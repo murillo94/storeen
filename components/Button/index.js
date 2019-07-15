97ada1f86d54da86d15086b91b76f90d7ab5b9a5
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-07-15 13:07:31 -0300

    Better styling button with icon

--- components/Button/index.js
+++ components/Button/index.js
@@ -8,17 +8,25 @@ const Button = ({
 }) => (
   <>
     <button type={type} onClick={onClick}>
-      {icon && <Icon name={icon} />}
-      {text}
+      {icon && <Icon name={icon} size={17} />}
+      <span>{text}</span>
     </button>
 
     <style jsx>
       {`
         button {
           font-size: 14px;
+          border: 1px solid gray;
           border-radius: 8px;
           padding: 10px;
           cursor: pointer;
+          display: inline-flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        span {
+          margin-left: 5px;
         }
       `}
     </style>
