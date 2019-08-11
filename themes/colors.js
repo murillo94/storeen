c817eaafbf20e6971870dbc91e53ed8444af91b0
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-08-11 19:49:41 -0300

    Creating theme

---
+++ themes/colors.js
@@ -0,0 +1,33 @@
+export const primary100 = '#bfcfff';
+export const primary200 = '#afc3ff';
+export const primary300 = '#9fb7ff';
+export const primary400 = '#8fabff';
+export const primary500 = '#7e9fff';
+export const primary600 = '#6f93ff';
+export const primary700 = '#5f87ff';
+
+export const negative100 = '#f4b6af';
+export const negative200 = '#f2a49b';
+export const negative300 = '#ef9187';
+export const negative400 = '#ec7f73';
+export const negative500 = '#ea6d5e';
+export const negative600 = '#e75b4b';
+export const negative700 = '#e54937';
+
+export const positive100 = '#9bdabd';
+export const positive200 = '#83d1ac';
+export const positive300 = '#6ac79c';
+export const positive400 = '#51be8b';
+export const positive500 = '#38b57a';
+export const positive600 = '#1fac6a';
+export const positive700 = '#07a35a';
+
+export const mono100 = '#dadada';
+export const mono200 = '#d1d1d1';
+export const mono300 = '#c7c7c7';
+export const mono400 = '#bebebe';
+export const mono500 = '#b5b5b5';
+export const mono600 = '#acacac';
+export const mono700 = '#a3a3a3';
+export const mono800 = '#727272';
+export const mono900 = '#333333';
