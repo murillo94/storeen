47c3788a661a05a9901a022e98f8b99411f04ec0
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-08-17 14:09:02 -0300

    Removing container

--- pages/promotions/index.js
+++ pages/promotions/index.js
@@ -2,14 +2,8 @@ import Router from 'next/router';
 
 import Page from 'layouts/admin/page';
 
-import Container from 'components/container';
 import SubHeader from 'components/subHeader';
 import Button from 'components/button';
-import { Heading, Paragraph } from 'components/typography';
-
-const headingStyle = {
-  margin: '0 0 20px'
-};
 
 const Promotions = () => {
   const handleAddPromotion = () => Router.push('/promotions/add');
@@ -19,15 +13,6 @@ const Promotions = () => {
       <SubHeader title="Promoções">
         <Button text="Nova promoção" onClick={handleAddPromotion} />
       </SubHeader>
-      <Container title="Geral">
-        <Heading
-          is="h3"
-          text="R$ 90,60 de desconto"
-          customStyle={headingStyle}
-        />
-        <Paragraph text="Não expira" />
-        <Paragraph text="Não tem valor de pedido" />
-      </Container>
     </Page>
   );
 };
