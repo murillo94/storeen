df279c8387d1c8b32d7009585b2459dd8b2b3272
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-08-19 21:38:50 -0300

    Adjusting padding

--- components/table/src/TableCell.js
+++ components/table/src/TableCell.js
@@ -11,7 +11,7 @@ const TableCell = ({ children, flexBasis = '' }) => (
           display: flex;
           align-items: center;
           flex: 1 1 0%;
-          padding-right: 10px;
+          padding-right: 20px;
         }
 
         .cellWidth {
