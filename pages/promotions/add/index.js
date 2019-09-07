c0cbfa0b5bf77bbaa6f74a1a3b86456eca182a8d
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-09-07 19:12:11 -0300

    Adding fields to add promotion page

--- pages/promotions/add/index.js
+++ pages/promotions/add/index.js
@@ -4,6 +4,7 @@ import Page from 'layouts/adminContent/page';
 
 import SubHeader from 'components/subHeader';
 import Container from 'components/container';
+import Checkbox from 'components/checkbox';
 
 const AddPromotion = () => {
   const handleBack = () => Router.push('/promotions');
@@ -14,8 +15,16 @@ const AddPromotion = () => {
       <Container title="Geral">todo</Container>
       <Container title="Aplicável">todo</Container>
       <Container title="Requisitos mínimos">todo</Container>
-      <Container title="Código promocional">todo</Container>
-      <Container title="Agendar">todo</Container>
+      <Container title="Código promocional" isForm>
+        <Checkbox
+          id="promotional-code"
+          name="promotional-code"
+          text="Habilitar código promocional?"
+        />
+      </Container>
+      <Container title="Agendar">
+        <Checkbox id="schedule" name="schedule" text="Agendar promoção?" />
+      </Container>
     </Page>
   );
 };
