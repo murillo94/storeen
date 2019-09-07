468d8e600c308343fca80ad4d7ba240f2e482e45
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-09-07 19:59:20 -0300

    Adding fields to add categories page

--- pages/categories/add/index.js
+++ pages/categories/add/index.js
@@ -4,6 +4,7 @@ import Page from 'layouts/adminContent/page';
 
 import SubHeader from 'components/subHeader';
 import Container from 'components/container';
+import Radio from 'components/radio';
 
 const AddCategorie = () => {
   const handleBack = () => Router.push('/categories');
@@ -12,7 +13,22 @@ const AddCategorie = () => {
     <Page>
       <SubHeader title="Nova categoria" hasBack onClick={handleBack} />
       <Container title="Geral">todo</Container>
-      <Container title="Tipo">todo</Container>
+      <Container title="Tipo" isForm>
+        <Radio
+          id="manual"
+          name="type"
+          value="manual"
+          text="Manual"
+          description="Escolha os produtos manualmente para essa coleção."
+        />
+        <Radio
+          id="automated"
+          name="type"
+          value="automated"
+          text="Automatizada"
+          description="Crie condições para que produtos existentes ou futuros sejam inseridos nessa coleção."
+        />
+      </Container>
     </Page>
   );
 };
