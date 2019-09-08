7054eb30fbb023349eafa6ba4d0193cfc237e203
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-09-08 18:19:58 -0300

    Adding fields

--- pages/settings/legal/index.js
+++ pages/settings/legal/index.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Page from 'layouts/adminSettingsContent/page';
 
 import Container from 'components/container';
@@ -5,60 +7,101 @@ import { Input } from 'components/input';
 import Footer from 'components/footer';
 import Button from 'components/button';
 
-const LegalSettings = () => (
-  <Page>
-    <Container
-      title="Política de reembolso"
-      subTitle="Uma boa política de devolução ou reembolso pode ajudar a proteger a sua empresa e ganhar a confiança dos clientes."
-      isForm
-    >
-      <Input
-        labelText="Descrição"
-        id="refund-description"
-        name="refund-description"
-      />
-      <Button appearance="minimal" text="Copiar modelo" />
-    </Container>
-    <Container
-      title="Política de privacidade"
-      subTitle="Garanta ao seu usuário o sigilo dos dados fornecidos."
-      isForm
-    >
-      <Input
-        labelText="Descrição"
-        id="privacy-description"
-        name="privacy-description"
-      />
-      <Button appearance="minimal" text="Copiar modelo" />
-    </Container>
-    <Container
-      title="Termos de serviço"
-      subTitle="Estabeleça o que os usuários podem e não podem fazer ao comprar em seu site."
-      isForm
-    >
-      <Input
-        labelText="Descrição"
-        id="service-description"
-        name="service-description"
-      />
-      <Button appearance="minimal" text="Copiar modelo" />
-    </Container>
-    <Container
-      title="Política de envio"
-      subTitle="Informe aos seus clientes como será o envio, controle e prazos para a entrega."
-      isForm
-    >
-      <Input
-        labelText="Descrição"
-        id="shipping-description"
-        name="shipping-description"
-      />
-      <Button appearance="minimal" text="Copiar modelo" />
-    </Container>
-    <Footer>
-      <Button text="Salvar" />
-    </Footer>
-  </Page>
-);
+const refundModel = 'Política de reembolso';
+const privacyModel = 'Política de privacidade';
+const serviceModel = 'Termos de serviço';
+const shippingModel = 'Política de envio';
+
+const LegalSettings = () => {
+  const [refundDescription, setRefundDescription] = useState('');
+  const [privacyDescription, setPrivacyDescription] = useState('');
+  const [serviceDescription, setServiceDescription] = useState('');
+  const [shippingDescription, setShippingDescription] = useState('');
+
+  const handleClickCopyRefundModel = () => setRefundDescription(refundModel);
+
+  const handleClickCopyPrivacyModel = () => setPrivacyDescription(privacyModel);
+
+  const handleClickCopyServiceModel = () => setServiceDescription(serviceModel);
+
+  const handleClickCopyShippingModel = () =>
+    setShippingDescription(shippingModel);
+
+  return (
+    <Page>
+      <Container
+        title="Política de reembolso"
+        subTitle="Uma boa política de devolução ou reembolso pode ajudar a proteger a sua empresa e ganhar a confiança dos clientes."
+        isForm
+      >
+        <Input
+          labelText="Descrição"
+          id="refund-description"
+          name="refund-description"
+          value={refundDescription}
+        />
+        <Button
+          appearance="minimal"
+          text="Copiar modelo"
+          onClick={handleClickCopyRefundModel}
+        />
+      </Container>
+      <Container
+        title="Política de privacidade"
+        subTitle="Garanta ao seu usuário o sigilo dos dados fornecidos."
+        isForm
+      >
+        <Input
+          labelText="Descrição"
+          id="privacy-description"
+          name="privacy-description"
+          value={privacyDescription}
+        />
+        <Button
+          appearance="minimal"
+          text="Copiar modelo"
+          onClick={handleClickCopyPrivacyModel}
+        />
+      </Container>
+      <Container
+        title="Termos de serviço"
+        subTitle="Estabeleça o que os usuários podem e não podem fazer ao comprar em seu site."
+        isForm
+      >
+        <Input
+          labelText="Descrição"
+          id="service-description"
+          name="service-description"
+          value={serviceDescription}
+        />
+        <Button
+          appearance="minimal"
+          text="Copiar modelo"
+          onClick={handleClickCopyServiceModel}
+        />
+      </Container>
+      <Container
+        title="Política de envio"
+        subTitle="Informe aos seus clientes como será o envio, controle e prazos para a entrega."
+        isForm
+      >
+        <Input
+          labelText="Descrição"
+          id="shipping-description"
+          name="shipping-description"
+          value={shippingDescription}
+        />
+        <Button
+          appearance="minimal"
+          text="Copiar modelo"
+          onClick={handleClickCopyShippingModel}
+        />
+      </Container>
+      <Footer>
+        <Button text="Salvar" />
+      </Footer>
+    </Page>
+  );
+};
 
 export default LegalSettings;
