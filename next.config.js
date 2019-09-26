701df9cf98e47709f398940f7e081e67c804d21f
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-09-26 00:13:11 -0300

    Removing next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,30 +1,21 @@
 /* eslint-disable no-param-reassign, dot-notation */
 const path = require('path');
-const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        handleImages: ['png', 'svg', 'ico'],
-        pngquant: {
-          speed: 10
-        },
-        svgo: {
-          plugins: [{ removeComments: true, cleanupAttrs: true }]
-        }
-      }
-    ]
-  ],
-  {
-    webpack(config) {
-      config.resolve.alias['components'] = path.join(__dirname, 'components');
-      config.resolve.alias['layouts'] = path.join(__dirname, 'layouts');
-      config.resolve.alias['themes'] = path.join(__dirname, 'themes');
-      config.resolve.alias['images'] = path.join(__dirname, 'static/images');
-      return config;
-    }
+module.exports = optimizedImages({
+  handleImages: ['png', 'svg', 'ico'],
+  pngquant: {
+    speed: 10
+  },
+  svgo: {
+    plugins: [{ removeComments: true, cleanupAttrs: true }]
+  },
+  target: 'serverless',
+  webpack(config) {
+    config.resolve.alias['components'] = path.join(__dirname, 'components');
+    config.resolve.alias['layouts'] = path.join(__dirname, 'layouts');
+    config.resolve.alias['themes'] = path.join(__dirname, 'themes');
+    config.resolve.alias['images'] = path.join(__dirname, 'static/images');
+    return config;
   }
-);
+});
