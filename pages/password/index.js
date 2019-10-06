8555b153c1b918022c425e9d8f1a795b9f91742c
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-10-06 17:09:53 -0300

    Adding default route to password

---
+++ pages/password/index.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import Router from 'next/router';
+
+export default class extends Component {
+  static async getInitialProps({ res }) {
+    const url = '/password/new';
+    if (res) {
+      res.writeHead(302, {
+        Location: url
+      });
+      res.end();
+    } else {
+      Router.push(url);
+    }
+
+    return {};
+  }
+}
