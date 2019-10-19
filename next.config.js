8905d707a1bc97f0cb6046d85ec30609bfda3495
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-10-19 18:27:53 -0300

    Creating src folder

--- next.config.js
+++ next.config.js
@@ -12,14 +12,14 @@ module.exports = optimizedImages({
   },
   target: 'serverless',
   webpack(config) {
-    config.resolve.alias['components'] = path.join(__dirname, 'components');
-    config.resolve.alias['containers'] = path.join(__dirname, 'containers');
-    config.resolve.alias['layouts'] = path.join(__dirname, 'layouts');
-    config.resolve.alias['hooks'] = path.join(__dirname, 'hooks');
-    config.resolve.alias['utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['components'] = path.join(__dirname, 'src/components');
+    config.resolve.alias['containers'] = path.join(__dirname, 'src/containers');
+    config.resolve.alias['layouts'] = path.join(__dirname, 'src/layouts');
+    config.resolve.alias['hooks'] = path.join(__dirname, 'src/hooks');
+    config.resolve.alias['utils'] = path.join(__dirname, 'src/utils');
     config.resolve.alias['images'] = path.join(
       __dirname,
-      'public/static/images'
+      'src/public/static/images'
     );
     return config;
   }
