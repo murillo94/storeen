9310622c51c864b3817aec10390c3ef3468f336e
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-10-27 19:46:07 -0300

    Creating initial payments page

--- src/pages/settings/payments/index.js
+++ src/pages/settings/payments/index.js
@@ -1,6 +1,7 @@
 import Page from 'layouts/adminSettingsContent/store';
 
 import Container from 'components/container';
+import { ImageDescription } from 'components/image';
 import Footer from 'components/footer';
 import Button from 'components/button';
 
@@ -12,7 +13,42 @@ const PaymentsSettings = () => (
       title="Meios de pagamento"
       subTitle="Selecione um meio de pagamento para receber os pagamentos."
     >
-      todo
+      <Container>
+        <ImageDescription
+          src={require('images/payments/mercadopago.svg')}
+          alt="Mercado Pago logo"
+          description="Mercado Pago"
+          width="32px"
+          height="32px"
+        />
+      </Container>
+      <Container>
+        <ImageDescription
+          src={require('images/payments/wirecard.svg')}
+          alt="Wirecard logo"
+          description="Wirecard"
+          width="32px"
+          height="32px"
+        />
+      </Container>
+      <Container>
+        <ImageDescription
+          src={require('images/payments/pagseguro.svg')}
+          alt="PagSeguro logo"
+          description="PagSeguro"
+          width="32px"
+          height="32px"
+        />
+      </Container>
+      <Container>
+        <ImageDescription
+          src={require('images/payments/paypal.svg')}
+          alt="PayPal logo"
+          description="PayPal"
+          width="32px"
+          height="32px"
+        />
+      </Container>
     </Container>
     <Footer>
       <Button text="Salvar" />
