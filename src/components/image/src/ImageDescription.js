e775bcdf64350f29dc154866f87b4fa397ce3268
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-10-27 20:54:05 -0300

    Creating object to style image description component

--- src/components/image/src/ImageDescription.js
+++ src/components/image/src/ImageDescription.js
@@ -1,11 +1,15 @@
 import { Text } from 'components/typography';
 import Image from './Image';
 
+const textStyle = {
+  marginLeft: '8px'
+};
+
 const ImageDescription = ({ src, alt, description, width, height }) => (
   <>
     <div>
       <Image src={src} alt={alt} width={width} height={height} />
-      <Text text={description} customStyle={{ marginLeft: '8px' }} />
+      <Text text={description} customStyle={textStyle} />
     </div>
 
     <style jsx>
