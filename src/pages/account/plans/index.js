58d4cdd2a92d604f20c9a1c5dc3d21d7355387c0
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-10-27 23:32:31 -0300

    Adding table to plans page

--- src/pages/account/plans/index.js
+++ src/pages/account/plans/index.js
@@ -1,11 +1,59 @@
 import Page from 'layouts/adminSettingsContent/account';
 
-import Container from 'components/container';
+import {
+  Table,
+  TableRow,
+  TableBodyCell,
+  TextTableCell
+} from 'components/table';
 
 import useLayout from 'hooks/layout/useLayout';
 
+const headers = ['Plano', 'Produtos', 'Preço'];
+const items = [
+  {
+    name: 'Grátis',
+    products: '2 produtos',
+    price: 'R$0'
+  },
+  {
+    name: 'Iniciante',
+    products: '5 produtos',
+    price: 'R$15'
+  },
+  {
+    name: 'Pequeno',
+    products: '15 produtos',
+    price: 'R$35'
+  },
+  {
+    name: 'Médio',
+    products: '20 produtos',
+    price: 'R$50'
+  },
+  {
+    name: 'Grande',
+    products: 'Ilimitado',
+    price: 'R$109'
+  }
+];
+
 const PlansAccountSettings = () => (
-  <Container title="Planos de cobrança">todo conteúdo e subtítulo</Container>
+  <Table title="Planos de cobrança" headers={headers}>
+    {items.map(item => (
+      <TableRow key={item.name}>
+        <TableBodyCell>
+          <TextTableCell text={item.name} />
+        </TableBodyCell>
+        <TableBodyCell>
+          <TextTableCell text={item.products} />
+        </TableBodyCell>
+        <TableBodyCell>
+          <TextTableCell text={item.price} />
+        </TableBodyCell>
+      </TableRow>
+    ))}
+  </Table>
 );
 
 export default useLayout(Page)(PlansAccountSettings);
