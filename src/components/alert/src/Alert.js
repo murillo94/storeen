2374152cc6c9285740852635b517fcbbdb9a90f5
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-11-03 09:54:40 -0300

    Better ux alert component

--- src/components/alert/src/Alert.js
+++ src/components/alert/src/Alert.js
@@ -1,29 +1,51 @@
-import { Heading } from 'components/typography';
+import { Heading, Paragraph } from 'components/typography';
 
 import useTheme from 'hooks/theme/useTheme';
 
 import { mono0, mono100 } from 'utils/theme/colors';
 import { radius600 } from 'utils/theme/radius';
 
-const Alert = ({ children, appearance = 'positive', title = '' }) => {
+const descriptionStyle = {
+  margin: '0'
+};
+
+const Alert = ({
+  children,
+  appearance = 'positive',
+  title = '',
+  description = ''
+}) => {
   const theme = useTheme(appearance);
 
   return (
     <>
-      <div>
+      <div className="container">
         {title && <Heading is="h4" text={title} />}
+        {description && (
+          <div className="description">
+            <Paragraph
+              text={description}
+              color="muted"
+              customStyle={descriptionStyle}
+            />
+          </div>
+        )}
         {children}
       </div>
 
       <style jsx>
         {`
-          div {
+          .container {
             background-color: ${mono0};
             border: 1px solid ${mono100};
             border-left: 4px solid ${theme.borderColor};
             border-radius: ${radius600};
             padding: 15px;
             margin-bottom: 20px;
+          }
+
+          .description {
+            margin-top: 10px;
           }
         `}
       </style>
