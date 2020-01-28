3f09f377d429a75e34564f8a30dc7517b0f33609
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-01-27 23:05:36 -0300

    Creating hook state to shipping page

---
+++ src/containers/useSettingsShipping.js
@@ -0,0 +1,17 @@
+import useForm from 'hooks/form/useForm';
+
+const useSettingsShipping = () =>
+  useForm({
+    local: {
+      active: false,
+      orientation: ''
+    },
+    postOffice: {
+      active: false,
+      codeAdmin: '',
+      password: '',
+      codeServices: ''
+    }
+  });
+
+export default useSettingsShipping;
--- src/pages/settings/shipping/index.js
+++ src/pages/settings/shipping/index.js
@@ -1,23 +1,124 @@
 import Page from 'layouts/adminSettingsContent/store';
 
 import Container from 'components/container';
+import Collapse from 'components/collapse';
+import { Paragraph } from 'components/typography';
+import { Form } from 'components/form';
+import Input from 'components/input';
+import Switch from 'components/switch';
 import Footer from 'components/footer';
 import Button from 'components/button';
 
+import useSettingsShipping from 'containers/useSettingsShipping';
+
 import useLayout from 'hooks/layout/useLayout';
 
-const ShippingSettings = () => (
-  <>
+const descriptionStyle = {
+  margin: '0 0 20px'
+};
+
+const ContainerShipping = ({ children, collapse }) => (
+  <Container padding="15px" margin="0 0 20px" hasBoxShadow={false} hasBorder>
     <Container
-      title="Opções de frete"
-      subTitle="Escolha as maneiras de levar seus produtos até seus clientes."
+      orientation="horizontal"
+      align="between"
+      padding="0"
+      margin="0"
+      hasBoxShadow={false}
+      hasBorder={false}
     >
-      todo
+      {children}
     </Container>
-    <Footer>
-      <Button text="Salvar" />
-    </Footer>
-  </>
+    {collapse}
+  </Container>
 );
+
+const ShippingSettings = () => {
+  const {
+    state: { local, postOffice },
+    actions: { onChange }
+  } = useSettingsShipping();
+
+  return (
+    <>
+      <Container
+        title="Opções de frete"
+        subTitle="Escolha as maneiras de levar seus produtos até seus clientes."
+      >
+        <ContainerShipping
+          collapse={
+            <Collapse isOpen={local.active}>
+              <Paragraph
+                text="Insira aqui as instruções que seus clientes devem seguir caso queiram retirar o produto pessoalmente."
+                color="muted"
+                customStyle={descriptionStyle}
+              />
+              <Input
+                labelText="Orientação para retirar pessoalmente"
+                id="local-orientation"
+                name="local.orientation"
+                value={local.orientation}
+                onChange={onChange}
+              />
+            </Collapse>
+          }
+        >
+          Habilitar Retirar Pessoalmente?
+          <Switch
+            id="local"
+            name="local.active"
+            checked={local.active}
+            onChange={onChange}
+          />
+        </ContainerShipping>
+        <ContainerShipping
+          collapse={
+            <Collapse isOpen={postOffice.active}>
+              <Paragraph
+                text="Exclusivo para lojistas que possuem contrato com os Correios."
+                color="muted"
+                customStyle={descriptionStyle}
+              />
+              <Form>
+                <Input
+                  labelText="Código administrativo do contrato"
+                  id="post-office-code-admin"
+                  name="postOffice.codeAdmin"
+                  value={postOffice.codeAdmin}
+                  onChange={onChange}
+                />
+                <Input
+                  labelText="Senha do contrato"
+                  id="post-office-password"
+                  name="postOffice.password"
+                  value={postOffice.password}
+                  onChange={onChange}
+                />
+                <Input
+                  labelText="Códigos dos serviços contratados"
+                  id="post-office-code-services"
+                  name="postOffice.codeServices"
+                  value={postOffice.codeServices}
+                  onChange={onChange}
+                />
+              </Form>
+            </Collapse>
+          }
+        >
+          Contrato Correios
+          <Switch
+            id="post-office"
+            name="postOffice.active"
+            checked={postOffice.active}
+            onChange={onChange}
+          />
+        </ContainerShipping>
+      </Container>
+      <Footer>
+        <Button text="Salvar" />
+      </Footer>
+    </>
+  );
+};
 
 export default useLayout(Page)(ShippingSettings);
