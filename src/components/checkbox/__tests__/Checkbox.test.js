9b921913e1632acf3864ed4c1f343583f39bc06f
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-02-12 20:43:19 -0300

    Testing checkbox component

--- src/components/checkbox/__tests__/Checkbox.test.js
+++ src/components/checkbox/__tests__/Checkbox.test.js
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { useState } from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Checkbox from '../index';
 
@@ -9,12 +10,51 @@ describe('Anchor', () => {
     expect(container).toBeInTheDocument();
   });
 
-  // appearance
-  // id
-  // name
-  // value
-  // text
-  // description
-  // checked
-  // onChange
+  test('should have text and description', () => {
+    const { getByLabelText, getByText } = render(
+      <Checkbox text="im text" description="im description" />
+    );
+
+    const text = getByLabelText('im text');
+    const description = getByText('im description');
+
+    expect(text).toBeInTheDocument();
+    expect(text.closest('label')).toHaveStyle('display: inline-flex;');
+    expect(description).toBeInTheDocument();
+    expect(description).toHaveStyle('margin: 5px 0 0;');
+    expect(description.closest('div')).toHaveClass('with-description');
+  });
+
+  test('should have change with checked', () => {
+    const onChange = jest.fn();
+    const Test = () => {
+      const [checked, setChecked] = useState(false);
+      return (
+        <Checkbox
+          checked={checked}
+          text="im text"
+          onChange={({ target }) => {
+            setChecked(target.checked);
+            onChange(target.checked);
+          }}
+        />
+      );
+    };
+
+    const { getByLabelText } = render(<Test />);
+    const checkbox = getByLabelText('im text');
+
+    expect(checkbox.checked).toBe(false);
+    expect(onChange).not.toBeCalled();
+
+    fireEvent.click(checkbox);
+
+    expect(checkbox.checked).toBe(true);
+    expect(onChange).toBeCalledWith(true);
+
+    fireEvent.click(checkbox);
+
+    expect(checkbox.checked).toBe(false);
+    expect(onChange).toBeCalledWith(false);
+  });
 });
