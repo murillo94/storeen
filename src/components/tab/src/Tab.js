a80bd398ac90e0e3595b9a754dccb1cb9bfb6b6a
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-02-18 23:28:16 -0300

    Refactoring tab component

--- src/components/tab/src/Tab.js
+++ src/components/tab/src/Tab.js
@@ -1,13 +1,52 @@
 import { withRouter } from 'next/router';
+import Link from 'next/link';
 
 import Icon from 'components/icon';
 import { Text } from 'components/typography';
-import TabLink from './TabLink';
-import TabButton from './TabButton';
+import Button from 'components/button';
 
 import useTheme from 'hooks/theme/useTheme';
 
+import { primary700 } from 'utils/theme/colors';
 import { radius600 } from 'utils/theme/radius';
+import { primary } from 'utils/theme/shadows';
+
+const buttonStyle = {
+  width: '100%'
+};
+
+const TabButton = ({ children, onClick }) => (
+  <Button hasBorder={false} customStyle={buttonStyle} onClick={onClick}>
+    {children}
+  </Button>
+);
+
+const TabLink = ({ children, href }) => (
+  <>
+    <Link href={href}>
+      <a>{children}</a>
+    </Link>
+
+    <style jsx>
+      {`
+        a {
+          color: inherit;
+          text-decoration: none;
+          padding: 8px 6px;
+          outline: 0;
+          width: 100%;
+          display: flex;
+          align-items: center;
+        }
+
+        :focus {
+          box-shadow: ${primary};
+          border-color: ${primary700};
+        }
+      `}
+    </style>
+  </>
+);
 
 const Content = ({ text, icon }) => (
   <>
