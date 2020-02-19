6f310131c0e5bd6c0fd9fa175cd7a6702c9d6bfb
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-02-19 00:05:17 -0300

    Starting test to tab component

--- src/components/tab/__tests__/Tab.test.js
+++ src/components/tab/__tests__/Tab.test.js
@@ -1,169 +1,60 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { Tab } from '../index';
 
-import {
-  primary700,
-  mono0,
-  mono900,
-  mono200,
-  negative700,
-  positive700
-} from 'utils/theme/colors';
-
-const content = 'im content';
+import { primary700, negative700, positive700 } from 'utils/theme/colors';
+import { radius600 } from 'utils/theme/radius';
 
 describe('Tab', () => {
   test('should render', () => {
     const { getByRole } = render(<Tab />);
 
-    expect(getByRole('button')).toBeInTheDocument();
+    expect(getByRole('tab')).toBeInTheDocument();
   });
 
-  test('should have children and default values', () => {
-    const { getByRole } = render(<Tab>{content}</Tab>);
+  test('should have text and icon', () => {});
 
-    const button = getByRole('button');
+  test('should have link', () => {});
 
-    expect(button).toHaveTextContent(content);
-    expect(button).toHaveAttribute('type', 'button');
-    expect(button).toHaveStyle(`border: 1px solid ${primary700}`);
-    expect(button).toHaveStyle('width: auto');
-    expect(button).not.toBeDisabled();
-  });
+  test('should have button', () => {});
 
-  test('should have type submit', () => {
-    const { getByRole } = render(<Tab type="submit">{content}</Tab>);
+  test('should have selected', () => {});
 
-    const button = getByRole('button');
-
-    expect(button).toHaveAttribute('type', 'submit');
-  });
-
-  test('should have text and icon', () => {
-    const { getByRole } = render(<Tab text="im button" icon="plus" />);
-
-    const text = getByRole('button');
-
-    expect(text).toMatchInlineSnapshot(`
-      <button
-        aria-disabled="false"
-        class="jsx-976006719 "
-        style="width: auto;"
-        type="button"
-      >
-        <svg
-          class="feather feather-plus "
-          fill="none"
-          height="17"
-          stroke="#ffffff"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          width="17"
-        >
-          <g>
-            <line
-              x1="12"
-              x2="12"
-              y1="5"
-              y2="19"
-            />
-            <line
-              x1="5"
-              x2="19"
-              y1="12"
-              y2="12"
-            />
-          </g>
-        </svg>
-        <span
-          class="jsx-2151508852"
-          style="margin: 0px 0px 0px 5px;"
-        >
-          im button
-        </span>
-      </button>
-    `);
-  });
-
-  test('should not have border', () => {
-    const { getByRole } = render(<Tab>{content}</Tab>);
-
-    const button = getByRole('button');
-
-    expect(button).toHaveStyle('border: none');
-  });
-
-  test('should be disabled', () => {
-    const { getByRole } = render(<Tab disabled>{content}</Tab>);
-
-    const button = getByRole('button');
-
-    expect(button).toBeDisabled();
-  });
-
-  test('should have custom style', () => {
-    const { getByRole } = render(
-      <Tab customStyle={{ margin: '10px', backgroundColor: 'red' }}>
-        {content}
-      </Tab>
-    );
-
-    const button = getByRole('button');
-
-    expect(button).toHaveStyle('margin: 10px; background-color: red');
-  });
-
-  test('should have click', () => {
-    const onClick = jest.fn();
-    const { getByRole } = render(<Tab onClick={onClick}>{content}</Tab>);
-
-    const button = getByRole('button');
-
-    fireEvent.click(button);
-
-    expect(onClick).toHaveBeenCalled();
-  });
+  test('should have selected with href', () => {});
 
   test('should have default theme', () => {
-    const { getByRole } = render(<Tab>{content}</Tab>);
+    const { getByRole } = render(<Tab appearance="default" />);
 
-    const button = getByRole('button');
+    const item = getByRole('tab');
 
-    expect(button).toHaveStyle(`color: ${mono0}`);
-    expect(button).toHaveStyle(`background-color: ${primary700}`);
-    expect(button).toHaveStyle(`border: 1px solid ${primary700}`);
+    expect(item).toHaveStyle(`background-color: ${primary700}`);
+    expect(item).toHaveStyle(`border-radius: ${radius600}`);
   });
 
-  test('should have minimal theme', () => {
-    const { getByRole } = render(<Tab appearance="minimal">{content}</Tab>);
+  test('should have default as minimal theme', () => {
+    const { getByRole } = render(<Tab />);
 
-    const button = getByRole('button');
+    const item = getByRole('tab');
 
-    expect(button).toHaveStyle(`color: ${mono900}`);
-    expect(button).toHaveStyle('background-color: transparent');
-    expect(button).toHaveStyle(`border: 1px solid ${mono200}`);
+    expect(item).toHaveStyle('background-color: transparent');
+    expect(item).toHaveStyle(`border-radius: ${radius600}`);
   });
 
   test('should have negative theme', () => {
-    const { getByRole } = render(<Tab appearance="negative">{content}</Tab>);
+    const { getByRole } = render(<Tab appearance="negative" />);
 
-    const button = getByRole('button');
+    const item = getByRole('tab');
 
-    expect(button).toHaveStyle(`color: ${mono0}`);
-    expect(button).toHaveStyle(`background-color: ${negative700}`);
-    expect(button).toHaveStyle(`border: 1px solid ${negative700}`);
+    expect(item).toHaveStyle(`background-color: ${negative700}`);
+    expect(item).toHaveStyle(`border-radius: ${radius600}`);
   });
 
   test('should have positive theme', () => {
-    const { getByRole } = render(<Tab appearance="positive">{content}</Tab>);
+    const { getByRole } = render(<Tab appearance="positive" />);
 
-    const button = getByRole('button');
+    const item = getByRole('tab');
 
-    expect(button).toHaveStyle(`color: ${mono0}`);
-    expect(button).toHaveStyle(`background-color: ${positive700}`);
-    expect(button).toHaveStyle(`border: 1px solid ${positive700}`);
+    expect(item).toHaveStyle(`background-color: ${positive700}`);
+    expect(item).toHaveStyle(`border-radius: ${radius600}`);
   });
 });
