e3a619ade303cb4c85b563e243ef929ee2f14128
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-02-21 18:10:40 -0300

    Testing portal component

---
+++ src/components/portal/__tests__/Portal.test.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+
+import Portal from '../index';
+
+const content = 'im portal';
+
+describe('Portal', () => {
+  test('should render', () => {
+    const { container } = render(<Portal />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('should have children and id', () => {
+    const { getByText } = render(<Portal id="test">{content}</Portal>);
+
+    expect(getByText(content).parentNode).toHaveAttribute('id', 'test');
+  });
+});
