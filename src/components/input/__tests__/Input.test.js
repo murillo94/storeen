ded633b9185fe151207a1d7ca05d22c8a547ef32
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-02-28 23:21:00 -0300

    Testing input component

--- src/components/input/__tests__/Input.test.js
+++ src/components/input/__tests__/Input.test.js
@@ -1,10 +1,84 @@
-import { render } from '@testing-library/react';
+import { useState, createRef } from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from '../index';
 
 describe('Input', () => {
   test('should render', () => {
+    const { getByRole } = render(<Input />);
+
+    expect(getByRole('textbox')).toBeInTheDocument();
+  });
+
+  test('should have type password', () => {
+    const onClick = jest.fn();
+    const { getByRole } = render(<Input type="password" onClick={onClick} />);
+
+    const input = getByRole('textbox');
+    const button = getByRole('button');
+
+    expect(input).toHaveAttribute('type', 'password');
+
+    fireEvent.click(button);
+
+    expect(input).toHaveAttribute('type', 'text');
+  });
+
+  test('should have inner ref', () => {
+    const ref = createRef();
+    const { getByRole } = render(<Input innerRef={ref} />);
+
+    ref.current.focus();
+
+    expect(getByRole('textbox')).toHaveFocus();
+  });
+
+  test('should have placeholder', () => {
+    const { getByPlaceholderText } = render(
+      <Input placeholder="im placeholder" />
+    );
+
+    expect(getByPlaceholderText('im placeholder')).toBeInTheDocument();
+  });
+
+  test('should have mask', () => {
     const { container } = render(<Input />);
+
+    expect(container).toBeInTheDocument();
+  });
+
+  test('should have change', () => {
+    const onChange = jest.fn();
+    const Test = () => {
+      const [value, setValue] = useState('im value');
+      return (
+        <Input
+          labelText="im label"
+          id="id-test"
+          value={value}
+          onChange={({ target }) => {
+            setValue(target.value);
+            onChange(target.value);
+          }}
+        />
+      );
+    };
+
+    const { getByLabelText } = render(<Test />);
+    const input = getByLabelText('im label');
+
+    expect(input.value).toBe('im value');
+    expect(onChange).not.toBeCalled();
+
+    fireEvent.change(input, { target: { value: 'im value changed' } });
+
+    expect(input.value).toBe('im value changed');
+    expect(onChange).toBeCalledWith('im value changed');
+  });
+
+  test('should have click', () => {
+    const onClick = jest.fn();
+    const { container } = render(<Input icon="plus" onCick={onClick} />);
 
     expect(container).toBeInTheDocument();
   });
