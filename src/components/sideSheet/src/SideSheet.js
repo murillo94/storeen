89bdcae8d85628c09ead4e8eec216b53811f44b5
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-04 01:21:09 -0300

    Creating side sheet component

--- src/components/sideSheet/src/SideSheet.js
+++ src/components/sideSheet/src/SideSheet.js
@@ -1,3 +1,51 @@
-const SideSheet = () => <div>SideSheet</div>;
+import Overlay from 'components/overlay';
+
+import { mono0, mono200 } from 'utils/theme/colors';
+
+const SideSheet = ({
+  children,
+  visible = false,
+  width = '180px',
+  onClick = null
+}) => (
+  <>
+    <Overlay
+      id="side-sheet"
+      visible={visible}
+      removeBodyScroll
+      onClick={onClick}
+    >
+      <nav className="side-sheet">{children}</nav>
+    </Overlay>
+
+    <style jsx>
+      {`
+        nav {
+          background-color: ${mono0};
+          border-right: 1px solid ${mono200};
+          text-align: center;
+          padding: 20px 0 10px;
+          width: ${width};
+          height: 100vh;
+          overflow: auto;
+          -webkit-overflow-scrolling: touch;
+          -ms-overflow-style: -ms-autohiding-scrollbar;
+          position: fixed;
+          top: 0;
+          left: 0;
+          transform: ${visible
+            ? 'transform: translate3d(0, 0, 0);'
+            : 'translate3d(-125px, 0, 0)'};
+          will-change: transform;
+          z-index: 2;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          flex-shrink: 0;
+        }
+      `}
+    </style>
+  </>
+);
 
 export default SideSheet;
