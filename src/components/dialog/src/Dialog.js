44c1a157de17c647afa20f53d4faefea314253ab
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-04 18:36:22 -0300

    Creating dialog component

--- src/components/dialog/src/Dialog.js
+++ src/components/dialog/src/Dialog.js
@@ -1,3 +1,44 @@
-const Dialog = () => <div>Dialog</div>;
+import Overlay from 'components/overlay';
+
+import { mono0, mono200 } from 'utils/theme/colors';
+
+const Dialog = ({
+  children,
+  visible = false,
+  width = '560px',
+  onClick = null
+}) => (
+  <>
+    <Overlay id="dialog" visible={visible} removeBodyScroll onClick={onClick}>
+      <div role="dialog" className="dialog">
+        {children}
+      </div>
+    </Overlay>
+
+    <style jsx>
+      {`
+        div {
+          background-color: ${mono0};
+          border-right: 1px solid ${mono200};
+          text-align: center;
+          padding: 20px 0 10px;
+          width: ${width};
+          max-height: calc(100vh - 56px);
+          overflow: auto;
+          -webkit-overflow-scrolling: touch;
+          -ms-overflow-style: -ms-autohiding-scrollbar;
+          position: fixed;
+          top: 0;
+          left: 0;
+          z-index: 2;
+          display: ${visible ? 'flex' : 'none'};
+          flex-direction: column;
+          justify-content: space-between;
+          flex-shrink: 0;
+        }
+      `}
+    </style>
+  </>
+);
 
 export default Dialog;
