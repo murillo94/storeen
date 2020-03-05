3c1af6f6bb1261fae739ba5b6217924a0a4f5ad6
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-05 00:07:40 -0300

    Testing dialog component

---
+++ src/components/dialog/__tests__/Dialog.test.js
@@ -0,0 +1,55 @@
+import { render, fireEvent } from '@testing-library/react';
+import preloadAll from 'jest-next-dynamic';
+
+import Dialog from '../index';
+
+const content = 'im content';
+
+describe('Dialog', () => {
+  beforeAll(async () => {
+    await preloadAll();
+  });
+
+  test('should render', () => {
+    const { container } = render(<Dialog />);
+
+    expect(container).toMatchInlineSnapshot(`<div />`);
+  });
+
+  test('should have children', () => {
+    const { getByRole } = render(<Dialog visible>{content}</Dialog>);
+
+    const dialog = getByRole('dialog');
+
+    expect(dialog).toHaveTextContent(content);
+    expect(dialog).toHaveClass('dialog');
+    expect(dialog).toBeVisible();
+  });
+
+  test('should have width', () => {
+    const { getByText } = render(
+      <Dialog visible width="100px">
+        {content}
+      </Dialog>
+    );
+
+    expect(getByText(content)).toHaveStyle('width: 100px;');
+  });
+
+  test('should have click', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <Dialog visible onClick={onClick}>
+        {content}
+      </Dialog>
+    );
+
+    const children = getByText(content);
+
+    expect(children).toBeInTheDocument(content);
+
+    fireEvent.click(children.nextElementSibling);
+
+    expect(onClick).toHaveBeenCalled();
+  });
+});
