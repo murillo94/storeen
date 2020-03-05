aa4c2f9f405fd305f8f3c8fec435112ab992c0d6
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-05 12:30:03 -0300

    Changing width default side sheet component

--- src/components/sideSheet/src/SideSheet.js
+++ src/components/sideSheet/src/SideSheet.js
@@ -5,7 +5,7 @@ import { mono0, mono200 } from 'utils/theme/colors';
 const SideSheet = ({
   children,
   visible = false,
-  width = '180px',
+  width = '620px',
   onClick = null
 }) => (
   <>
@@ -25,7 +25,7 @@ const SideSheet = ({
           border-right: 1px solid ${mono200};
           text-align: center;
           padding: 20px 0 10px;
-          width: ${width};
+          max-width: ${width};
           height: 100vh;
           overflow: auto;
           -webkit-overflow-scrolling: touch;
