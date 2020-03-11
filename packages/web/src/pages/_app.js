70194ead188672a9991fe3bb03149c34394dd037
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-11 15:36:56 -0300

    Removing getInitialProps _app.js

--- packages/web/src/pages/_app.js
+++ packages/web/src/pages/_app.js
@@ -1,28 +1,12 @@
-import React from 'react';
-import App from 'next/app';
 import Head from 'next/head';
 
-export default class CustomApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+const CustomApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Storeen</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <Head>
-          <title>Storeen</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
-}
+export default CustomApp;
