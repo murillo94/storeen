239957bd7f5873e9c4bfa9f4c89d8f036fc8f5e8
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-23 23:08:43 -0300

    Renaming docs-pages to index

--- packages/styleguide/src/pages/index.js
+++ packages/styleguide/src/pages/index.js
@@ -1,6 +1,6 @@
+/* eslint-disable import/no-unresolved, no-underscore-dangle */
 import { TabList, Tab } from '@storeen/components';
 
-// eslint-disable-next-line import/no-unresolved
 import { frontMatter as docsPages } from './docs/*.mdx';
 
 const formatPath = path => path.replace(/\.mdx$/, '');
@@ -10,8 +10,11 @@ const App = () => (
     <h1>Storeen&apos;s design system</h1>
     <TabList>
       {docsPages.map(page => (
-        // eslint-disable-next-line no-underscore-dangle
-        <Tab text={page.title} href={formatPath(page.__resourcePath)} />
+        <Tab
+          key={page.__resourcePath}
+          text={page.title}
+          href={formatPath(page.__resourcePath)}
+        />
       ))}
     </TabList>
   </>
