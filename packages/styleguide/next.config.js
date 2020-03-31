7991643d9f0bd25b36978dd5feb7d2781bf6062f
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-03-31 00:59:08 -0300

    Adding href to frontMatter

--- packages/styleguide/next.config.js
+++ packages/styleguide/next.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable no-param-reassign, no-underscore-dangle */
 const withPlugins = require('next-compose-plugins');
 const withOptimizedImages = require('next-optimized-images');
 const withTranspileModules = require('next-transpile-modules')([
@@ -7,13 +7,24 @@ const withTranspileModules = require('next-transpile-modules')([
 ]);
 const withMdxEnhanced = require('next-mdx-enhanced');
 
+const getHref = resourcePath => {
+  const parts = resourcePath.split('/');
+  const partsLength = parts.length;
+  const fileName = parts[partsLength - 1];
+  const pathName = parts[partsLength - 2];
+
+  return `/${pathName}/${fileName.replace(/\.mdx$/, '')}`;
+};
+
 const plugins = [
   [
     withMdxEnhanced({
       layoutPath: 'src/layouts',
       defaultLayout: true,
       extendFrontMatter: {
-        phase: 'prebuild|loader|both'
+        process: (_, frontMatter) => ({
+          href: getHref(frontMatter.__resourcePath)
+        })
       }
     })({
       pageExtensions: ['js', 'jsx', 'md', 'mdx']
