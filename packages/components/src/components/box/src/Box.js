55b40cc0c108313de78c2600a708dd2da938dc2d
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-05-12 22:02:05 -0300

    Accepting variant as a prop

--- packages/components/src/components/box/src/Box.js
+++ packages/components/src/components/box/src/Box.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
-import { margin, padding, sx } from '@storeen/system';
+import { margin, padding, sx, variant } from '@storeen/system';
 
 import { theming } from '../../../theme/theming';
 
-export const Box = styled.div`${sx(theming)} ${margin} ${padding}`;
+// dar for no restante das variants e spread para juntar todos os css
+export const Box = styled.div`${sx(theming)} ${margin} ${padding} ${props => {
+  if (props.styleConfig && props.styleConfig.variants) {
+    const key = Object.keys(props.styleConfig.variants)[0];
+
+    return variant({
+      [key]: props.styleConfig.variants[key]
+    })(props);
+  }
+}}`;
