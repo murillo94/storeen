d5001ff86640594f0d7ea58e402dc378a7fb36dc
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-05-15 01:02:38 -0300

    Importing all components in styleguide

--- packages/styleguide/src/pages/_app.js
+++ packages/styleguide/src/pages/_app.js
@@ -1,45 +1,43 @@
 import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
 
-import {
-  Heading,
-  Paragraph,
-  Anchor,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableHeadCell,
-  TableBodyCell,
-  ThemeProvider
-} from '@storeen/components';
+import * as SC from '@storeen/components';
 import { PropsTable } from '../components/PropsTable';
 
 const customComponents = {
-  h1: props => <Heading {...props} is="h1" marginTop={11} marginBottom={5} />,
-  h2: props => <Heading {...props} is="h2" marginTop={11} marginBottom={5} />,
-  h3: props => <Heading {...props} is="h3" marginTop={11} marginBottom={5} />,
-  h4: props => <Heading {...props} is="h4" marginTop={11} marginBottom={5} />,
-  p: props => <Paragraph marginBottom={2} {...props} />,
-  a: props => <Anchor {...props} />,
-  table: Table,
-  thead: TableHead,
-  tbody: TableBody,
-  tr: TableRow,
-  th: TableHeadCell,
-  td: TableBodyCell,
+  ...SC,
+  h1: props => (
+    <SC.Heading {...props} is="h1" marginTop={11} marginBottom={5} />
+  ),
+  h2: props => (
+    <SC.Heading {...props} is="h2" marginTop={11} marginBottom={5} />
+  ),
+  h3: props => (
+    <SC.Heading {...props} is="h3" marginTop={11} marginBottom={5} />
+  ),
+  h4: props => (
+    <SC.Heading {...props} is="h4" marginTop={11} marginBottom={5} />
+  ),
+  p: props => <SC.Paragraph marginBottom={2} {...props} />,
+  a: props => <SC.Anchor {...props} />,
+  table: SC.Table,
+  thead: SC.TableHead,
+  tbody: SC.TableBody,
+  tr: SC.TableRow,
+  th: SC.TableHeadCell,
+  td: SC.TableBodyCell,
   PropsTable
 };
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider>
+  <SC.ThemeProvider>
     <MDXProvider components={customComponents}>
       <Head>
         <title>Storeen&apos;s design system</title>
       </Head>
       <Component {...pageProps} />
     </MDXProvider>
-  </ThemeProvider>
+  </SC.ThemeProvider>
 );
 
 export default App;
