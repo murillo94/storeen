33c67a94e9e1dae5c6028960a845be1b73031a59
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-05-14 23:28:15 -0300

    Adding variants to box component

--- packages/components/src/components/box/src/Box.js
+++ packages/components/src/components/box/src/Box.js
@@ -1,16 +1,14 @@
 import styled from 'styled-components';
 
-import { margin, padding, sx, variant } from '@storeen/system';
+import { margin, padding, sx } from '@storeen/system';
 
 import { theming } from '../../../theme/theming';
 
-// dar for no restante das variants e spread para juntar todos os css
-export const Box = styled.div`${sx(theming)} ${margin} ${padding} ${props => {
-  if (props.styleConfig && props.styleConfig.variants) {
-    const key = Object.keys(props.styleConfig.variants)[0];
+import { getVariant } from '../utils/variant';
 
-    return variant({
-      [key]: props.styleConfig.variants[key]
-    })(props);
-  }
-}}`;
+export const Box = styled.div`
+  ${sx(theming)}
+  ${margin}
+  ${padding}
+  ${getVariant}
+`;
