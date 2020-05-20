e8df2d449a27f68eb7a188b399ac8c4e8a1c945a
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-05-20 12:58:00 -0300

    Removing useless props

--- packages/components/src/components/container/src/Container.js
+++ packages/components/src/components/container/src/Container.js
@@ -8,91 +8,77 @@ export const Container = ({
   subTitle = '',
   orientation = '',
   align = '',
-  hasBoxShadow = false,
-  hasBorder = true,
   ...props
-}) => {
-  const withBorder = hasBorder
-    ? {
+}) => (
+  <>
+    <Box
+      as="section"
+      className={`${orientation} ${align}`}
+      padding={4}
+      marginBottom={9}
+      styleConfig={{
+        backgroundColor: 'gray0',
         borderWidth: '1px',
         borderStyle: 'solid',
-        borderColor: 'gray300'
-      }
-    : {};
-
-  return (
-    <>
-      <Box
-        as="section"
-        className={`${orientation} ${align}`}
-        padding={4}
-        marginBottom={9}
-        styleConfig={{
-          backgroundColor: 'gray0',
-          boxShadow: `${
-            hasBoxShadow &&
-            '0 0 0 1px rgba(63, 63, 68, 0.04), 0 1px 3px 0 rgba(63, 63, 68, 0.15)'
-          }`,
-          ...withBorder,
-          borderRadius: 3,
-          width: '100%',
-          overflow: 'hidden',
-          '&:last-of-type': {
-            marginBottom: 6
-          }
-        }}
-        {...props}
-      >
-        {title && (
+        borderColor: 'gray300',
+        borderRadius: 3,
+        width: '100%',
+        overflow: 'hidden',
+        '&:last-of-type': {
+          marginBottom: 6
+        }
+      }}
+      {...props}
+    >
+      {title && (
+        <Box
+          as="header"
+          padding={props.padding === 0 ? 4 : props.padding}
+          paddingBottom={4}
+        >
           <Box
-            as="header"
-            padding={props.padding === 0 ? 4 : props.padding}
-            paddingBottom={4}
+            styleConfig={{ display: 'flex', justifyContent: 'space-between' }}
           >
-            <Box
-              styleConfig={{ display: 'flex', justifyContent: 'space-between' }}
-            >
-              <Heading is="h2">{title}</Heading>
-              {optionsTitle}
-            </Box>
-            {subTitle && (
-              <Paragraph color="muted" marginTop={1} marginBottom={2}>
-                {subTitle}
-              </Paragraph>
-            )}
+            <Heading is="h2">{title}</Heading>
+            {optionsTitle}
           </Box>
-        )}
-        {children}
-      </Box>
+          {subTitle && (
+            <Paragraph color="muted" marginTop={1} marginBottom={2}>
+              {subTitle}
+            </Paragraph>
+          )}
+        </Box>
+      )}
+      {children}
+    </Box>
 
-      <style jsx>
-        {`
-          .vertical {
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-          }
+    <style jsx>
+      {`
+        .vertical {
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+        }
 
-          .horizontal {
-            display: flex;
-            align-items: center;
-            flex-direction: row;
-            flex-wrap: wrap;
-          }
+        .horizontal {
+          display: flex;
+          align-items: center;
+          flex-direction: row;
+          flex-wrap: wrap;
+        }
 
-          .right {
-            justify-content: flex-end;
-          }
+        .right {
+          justify-content: flex-end;
+        }
 
-          .left {
-            justify-content: flex-start;
-          }
+        .left {
+          justify-content: flex-start;
+        }
 
-          .between {
-            justify-content: space-between;
-          }
-        `}
-      </style>
-    </>
-  );
-};
+        .between {
+          justify-content: space-between;
+        }
+      `}
+    </style>
+  </>
+);
