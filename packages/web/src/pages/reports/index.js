4bb5c269141ee45312ebf35f596f14a691c1a74f
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-12-01 21:42:00 -0300

    Create reports page

--- packages/web/src/pages/reports/index.js
+++ packages/web/src/pages/reports/index.js
@@ -1,9 +1,24 @@
 import Page from '@web/layouts/adminContent';
 
-import { SubHeader } from '@storeen/components';
+import { SubHeader, Container, Stack, Inline } from '@storeen/components';
 
 import useLayout from '@web/hooks/layout/useLayout';
 
-const Reports = () => <SubHeader title="Relatórios" marginBottom={8} />;
+const Reports = () => (
+  <>
+    <SubHeader title="Relatórios" marginBottom={8} />
+    <Stack space="xlarge">
+      <Inline>
+        <Container title="Venda geral">todo</Container>
+        <Container title="Total de pedido">todo</Container>
+      </Inline>
+      <Inline>
+        <Container title="Média do valor dos pedidos">todo</Container>
+        <Container title="Total de visitas">todo</Container>
+        <Container title="Páginas mais visitadas">todo</Container>
+      </Inline>
+    </Stack>
+  </>
+);
 
 export default useLayout(Page)(Reports);
