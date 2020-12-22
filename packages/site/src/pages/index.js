324a0a81c9cfcb962f25dd14b0d02c3ec48d0fc9
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2020-12-22 13:05:08 -0300

    Create initial site texts

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -1,5 +1,51 @@
-import { Heading } from '@storeen/components';
+import {
+  Box,
+  Inline,
+  Anchor,
+  Image,
+  Heading,
+  Paragraph,
+  Button
+} from '@storeen/components';
 
-const Home = () => <Heading>hi :)</Heading>;
+const Home = () => (
+  <Box as="main">
+    <Box as="nav" padding={6}>
+      <Inline>
+        <Inline isProportional={false} sx={{ justifyContent: 'center' }}>
+          <Anchor href="#">funcionalidades</Anchor>
+          <Anchor href="#">planos</Anchor>
+        </Inline>
+        <Image
+          src={require('@site/public/images/logo-all-horizontal.svg')}
+          alt="Storeen logo com nome horizontal"
+          width="200px"
+          height="45px"
+          marginX="auto"
+          sx={{
+            display: ['block']
+          }}
+        />
+        <Inline isProportional={false} sx={{ justifyContent: 'center' }}>
+          <Anchor href="https://app-storeen.vercel.app/">entrar</Anchor>
+          <Anchor href="#">criar loja</Anchor>
+        </Inline>
+      </Inline>
+    </Box>
+    <Box as="section" padding={6} sx={{ textAlign: 'center' }}>
+      <Heading marginBottom={4}>
+        Crie uma loja virtual completa e sem mensalidade.
+      </Heading>
+      <Paragraph marginBottom={10}>
+        Sem limites de acessos, fotos e produtos. Nós cuidamos da tecnologia e
+        você só precisa se preocupar em vender.
+      </Paragraph>
+      <Inline isProportional={false} sx={{ justifyContent: 'center' }}>
+        <Button appearance="minimal">ver exemplo</Button>
+        <Button>criar minha loja agora</Button>
+      </Inline>
+    </Box>
+  </Box>
+);
 
 export default Home;
