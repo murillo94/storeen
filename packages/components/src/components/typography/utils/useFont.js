4ff763ee51daa3387ec35db2525eb3a1b29eec29
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2021-01-04 23:17:22 -0300

    Add colors to typography component

--- packages/components/src/components/typography/utils/useFont.js
+++ packages/components/src/components/typography/utils/useFont.js
@@ -3,7 +3,9 @@ import { theming } from '../../../theme/theming';
 const fonts = {
   default: theming.colors.gray900,
   muted: theming.colors.gray700,
-  primary: theming.colors.blue700
+  primary: theming.colors.blue700,
+  positive: theming.colors.green700,
+  negative: theming.colors.red700
 };
 
 const useFont = color => fonts[color] || color;
