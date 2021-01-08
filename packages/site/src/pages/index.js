4414848e8825c3ed758fccca9ce8f28e183701e1
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2021-01-07 23:38:01 -0300

    Improve text pricing page site

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -118,6 +118,23 @@ const Home = () => (
             }}
             padding={4}
           >
+            <Heading is="h5">Site próprio</Heading>
+            <Icon name="database" size={30} />
+          </Box>
+          <Box
+            sx={{
+              textAlign: 'left',
+              backgroundColor: 'gray0',
+              boxShadow: '0px 2px 40px rgba(0, 0, 0, 0.15)',
+              borderRadius: 4,
+              justifyContent: 'space-between',
+              flexDirection: 'column',
+              display: 'flex',
+              width: ['100%', '160px'],
+              height: ['auto', '160px']
+            }}
+            padding={4}
+          >
             <Heading is="h5">Acompanhe seus pedidos</Heading>
             <Icon name="package" size={30} />
           </Box>
@@ -137,23 +154,6 @@ const Home = () => (
           >
             <Heading is="h5">Pagamento integrado</Heading>
             <Icon name="credit-card" size={30} />
-          </Box>
-          <Box
-            sx={{
-              textAlign: 'left',
-              backgroundColor: 'gray0',
-              boxShadow: '0px 2px 40px rgba(0, 0, 0, 0.15)',
-              borderRadius: 4,
-              justifyContent: 'space-between',
-              flexDirection: 'column',
-              display: 'flex',
-              width: ['100%', '160px'],
-              height: ['auto', '160px']
-            }}
-            padding={4}
-          >
-            <Heading is="h5">Crie cupons</Heading>
-            <Icon name="gift" size={30} />
           </Box>
           <Box
             sx={{
@@ -226,8 +226,8 @@ const Home = () => (
             }}
             padding={4}
           >
-            <Heading is="h5">Altíssima velocidade</Heading>
-            <Icon name="upload-cloud" size={30} />
+            <Heading is="h5">SEO</Heading>
+            <Icon name="share-2" size={30} />
           </Box>
           <Box
             sx={{
--- packages/site/src/pages/pricing.js
+++ packages/site/src/pages/pricing.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 
 import {
   Box,
-  Inline,
   Heading,
+  Switch,
+  Inline,
   Paragraph,
-  Button,
   Stack,
   Icon,
-  Switch,
+  Button,
   theming
 } from '@storeen/components';
 
@@ -23,209 +23,296 @@ const Pricing = () => {
   const [plan, setPlan] = useState(TYPE.monthly);
 
   return (
-    <Box as="section" paddingX={3} paddingY={4} marginBottom={15}>
-      <Heading is="h2" marginBottom={4}>
-        Planos
-      </Heading>
-      <Paragraph marginBottom={6}>
-        Preço simples, resultados massivos. Venda todos os dias por menos de R$
-        2 por dia.
-      </Paragraph>
-      <Box marginBottom={4}>
-        <Switch
-          id="type"
-          name="isAnnually"
-          value="isAnnually"
-          leftText="mensal"
-          rightText="anual"
-          isChecked={plan === TYPE.annually}
-          onChange={() => {
-            setPlan(prevPlan =>
-              prevPlan === TYPE.monthly ? TYPE.annually : TYPE.monthly
-            );
-          }}
-        />
+    <>
+      <Box as="section" paddingX={3} paddingY={4} marginBottom={15}>
+        <Heading is="h2" marginBottom={4}>
+          Planos
+        </Heading>
+        <Paragraph marginBottom={6}>
+          Preço simples, resultados massivos. Venda todos os dias por menos de
+          R$ 2 por dia.
+        </Paragraph>
+        <Box marginBottom={4}>
+          <Switch
+            id="type"
+            name="isAnnually"
+            value="isAnnually"
+            leftText="mensal"
+            rightText="anual"
+            isChecked={plan === TYPE.annually}
+            onChange={() => {
+              setPlan(prevPlan =>
+                prevPlan === TYPE.monthly ? TYPE.annually : TYPE.monthly
+              );
+            }}
+          />
+        </Box>
+        <Paragraph marginBottom={8} color="positive">
+          Economize 20% (R$ 143,76) no plano anual!
+        </Paragraph>
+        <Box>
+          <Box
+            sx={{
+              backgroundColor: 'gray0',
+              boxShadow: '0 4px 6px 0 rgba(0, 0, 0, 0.1)',
+              borderWidth: '1px',
+              borderStyle: 'solid',
+              borderColor: 'gray300',
+              borderRadius: 4,
+              justifyContent: 'space-between',
+              flexDirection: 'column',
+              display: 'flex',
+              width: ['80%', '350px']
+            }}
+            padding={6}
+            marginX="auto"
+          >
+            <Stack space="large">
+              <Box>
+                <Heading is="h3" marginBottom={3}>
+                  {plan === TYPE.monthly ? 'Mensal' : 'Anual'}
+                </Heading>
+                <Heading is="h2">
+                  R$ {plan === TYPE.monthly ? '59,90' : '575,04'}
+                </Heading>
+              </Box>
+              <Stack space="xsmall">
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Suporte exclusivo por chat</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Produtos ilimitados</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Imagens ilimitados</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Acessos ilimitadas</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Certificado de segurança SSL</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Otimizações de SEO para produtos</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Checkout transparente</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Utilização de domínio próprio</Paragraph>
+                </Inline>
+                <Inline
+                  isProportional={false}
+                  isCollapse={false}
+                  sx={{
+                    '> p': {
+                      marginLeft: 2
+                    }
+                  }}
+                >
+                  <Icon
+                    name="check-circle"
+                    color={theming.colors.blue700}
+                    size={24}
+                  />
+                  <Paragraph>Estatísticas em tempo real</Paragraph>
+                </Inline>
+              </Stack>
+              <Box>
+                <Button marginBottom={2}>criar minha loja agora</Button>
+                <Paragraph color="muted">Teste gratuito por 14 dias</Paragraph>
+              </Box>
+            </Stack>
+          </Box>
+        </Box>
       </Box>
-      <Paragraph marginBottom={8} color="positive">
-        Economize 20% (R$ 143,76) no plano anual!
-      </Paragraph>
-      <Box>
-        <Box
+      <Box as="section" paddingX={3} paddingY={4} marginBottom={15}>
+        <Heading is="h2" marginBottom={10}>
+          Perguntas frequentes sobre o plano
+        </Heading>
+        <Stack space="xlarge" sx={{ textAlign: 'justify' }}>
+          <Box>
+            <Heading is="h3" marginBottom={2}>
+              Como funciona o pagamento do plano?
+            </Heading>
+            <Paragraph>
+              Existem dois métodos de pagamento o mensal e o anual, optando pelo
+              anual você conta com desconto extra na contratação da sua loja
+              virtual.
+            </Paragraph>
+          </Box>
+          <Box>
+            <Heading is="h3" marginBottom={2}>
+              Em quanto tempo minha loja virtual entra no ar?
+            </Heading>
+            <Paragraph>
+              Após a confirmação do pagamento via cartão, transferência ou
+              pagamentos via boleto a sua loja estará no ar instantaneamente.
+            </Paragraph>
+          </Box>
+          <Box>
+            <Heading is="h3" marginBottom={2}>
+              É possível gerenciar minha loja usando celular?
+            </Heading>
+            <Paragraph>
+              Sim! você pode usar o nosso site normalmente pois damos suporte
+              responsivo para qualquer tipo de dispositivo para que você consiga
+              acompanhar a performance da sua loja de qualquer lugar.
+            </Paragraph>
+          </Box>
+          <Box>
+            <Heading is="h3" marginBottom={2}>
+              Como funciona o sistema de frete?
+            </Heading>
+            <Paragraph>
+              O frete é calculado de acordo com o CEP que você(Vendedor)
+              cadastrou e com o CEP que o cliente digita no site para efetuar a
+              compra. Por padrão o app já vem com o PAC e SEDEX habilitados. É
+              possível também personalizar o prazo de entrega dos produtos,
+              criar fretes personalizados por cidade, estado ou para o país e
+              também habilitar a retirada do produto na loja.
+            </Paragraph>
+          </Box>
+          <Box>
+            <Heading is="h3" marginBottom={2}>
+              Preciso de CNPJ para criar uma loja?
+            </Heading>
+            <Paragraph>
+              Não precisa! Você consegue criar a sua loja na Bagy mesmo sendo
+              pessoa física.
+            </Paragraph>
+          </Box>
+          <Box>
+            <Heading is="h3" marginBottom={2}>
+              A Storeen disponibiliza um teste grátis?
+            </Heading>
+            <Paragraph>
+              O nosso plano fornece um período experimental de 14 dias para uso
+              da plataforma sem qualquer tipo de cobrança.
+            </Paragraph>
+          </Box>
+        </Stack>
+      </Box>
+      <Box as="section" paddingX={3} paddingY={4} marginBottom={15}>
+        <Inline
           sx={{
             backgroundColor: 'gray0',
-            boxShadow: '0 4px 6px 0 rgba(0, 0, 0, 0.1)',
             borderWidth: '1px',
             borderStyle: 'solid',
             borderColor: 'gray300',
-            borderRadius: 4,
-            justifyContent: 'space-between',
-            flexDirection: 'column',
-            display: 'flex',
-            width: ['80%', '350px']
+            borderRadius: 4
           }}
           padding={6}
-          marginX="auto"
         >
-          <Stack space="large">
-            <Box>
-              <Heading is="h3" marginBottom={3}>
-                {plan === TYPE.monthly ? 'Mensal' : 'Anual'}
-              </Heading>
-              <Heading is="h2">
-                R$ {plan === TYPE.monthly ? '59,90' : '575,04'}
-              </Heading>
-            </Box>
-            <Stack space="xsmall">
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Suporte exclusivo por chat</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Produtos ilimitados</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Imagens ilimitados</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Acessos ilimitadas</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Certificado de segurança SSL</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Otimizações de SEO para produtos</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Checkout transparente</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Utilização de domínio próprio</Paragraph>
-              </Inline>
-              <Inline
-                isProportional={false}
-                isCollapse={false}
-                sx={{
-                  '> p': {
-                    marginLeft: 2
-                  }
-                }}
-              >
-                <Icon
-                  name="check-circle"
-                  color={theming.colors.blue700}
-                  size={24}
-                />
-                <Paragraph>Estatísticas em tempo real</Paragraph>
-              </Inline>
-            </Stack>
-            <Button>criar minha loja agora</Button>
-          </Stack>
-        </Box>
+          <Heading is="h3">Experimente a Storeen grátis por 14 dias</Heading>
+          <Button>Começar</Button>
+        </Inline>
       </Box>
-    </Box>
+    </>
   );
 };
 
